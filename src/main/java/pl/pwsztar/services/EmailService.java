a8295770d2b3dc24f98db46b78fec0500b1eeee1
Tomek <tom.chr22@gmail.com>
2017-05-23 10:29:00 +0200

    email service added

---
+++ src/main/java/pl/pwsztar/services/EmailService.java
@@ -0,0 +1,62 @@
+package pl.pwsztar.services;
+
+import org.springframework.stereotype.Service;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+/**
+ * Created by Lapek on 19.05.2017.
+ */
+@Service
+public class EmailService {
+    protected String mailSmtpAuth = "true";
+    protected String mailSmtpHost = "smtp.gmail.com";
+    protected String mailSmtpPort = "587";
+    protected String mailSmtpStarttlsEnable = "true";
+    protected String mailEmailFrom = "dzialpomocy.pwsztar@gmail.com ";
+    protected String username = "dzialpomocy.pwsztar@gmail.com";
+    protected String password = "1qazxsw23edc";
+
+
+/*    public static void main(String[] args) {
+        EmailService emailService = new EmailService();
+        emailService.sendEmail("nerq95@gmail.com","Temat maila", "Tresc miala");
+
+    }*/
+
+    public boolean sendEmail(String recipientEmail, String subject, String content) {
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", mailSmtpAuth);
+        props.put("mail.smtp.starttls.enable", mailSmtpStarttlsEnable);
+        props.put("mail.smtp.host", mailSmtpHost);
+        props.put("mail.smtp.port", mailSmtpPort);
+        props.put("mail.smtp.ssl.trust", mailSmtpHost);
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(mailEmailFrom));
+            message.setRecipients(Message.RecipientType.TO,	InternetAddress.parse(recipientEmail));
+            message.setSubject(subject);
+            message.setText(content);
+
+            Transport.send(message);
+            return true;
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+}
